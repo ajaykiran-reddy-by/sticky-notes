af99a46ac176897898ebe234c9eec239ad186ce0
AjayKiran Reddy <ajaykiran.reddy@blueyonder.com>
2024-09-13 13:10:22 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#41f2d5",
+    },
+    secondary: {
+      main: "#556cd6",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
