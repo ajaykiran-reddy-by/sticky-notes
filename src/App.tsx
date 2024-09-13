79aaa514754a0f760b8e4800884114ecf5d35e10
Arkanti Nikhil Teja (CW) <Arkanti.Nikhilteja@blueyonder.com>
2024-09-13 15:03:20 +0530

    Added 'Add todo Dialog'

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
+import React, { useState } from 'react';   
 import {
   AppBar,
-  Box,
   Button,
   Grid2,
-  IconButton,
-  Toolbar,
   Typography,
 } from "@mui/material";
-import "./App.css";
-import { Menu } from "@mui/icons-material";
+import TodoCard from "./components/TodoCard";
 
 function App() {
+  const [openDialog, setOpenDialog] = useState(false);  
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);   
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);  
+  };
+
   return (
-    <AppBar style={{ width: "100%", padding: "0.5rem" }} position="static">
-      <Grid2
-        container
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Grid2 size={4}>
-          <Typography variant="h6" color="inherit" component="div">
-            Team A
-          </Typography>
+    <React.Fragment>
+      <AppBar style={{ width: "100%", padding: "0.5rem" }} position="static">
+        <Grid2
+          container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Grid2 size={4}>
+            <Typography variant="h6" color="inherit" component="div">
+              Team A  
+            </Typography>
+          </Grid2>
+          <Grid2 size={8} style={{ textAlign: "end" }}>
+            <Button variant="outlined" color="secondary" onClick={handleOpenDialog}>
+              Add Todo
+            </Button>
+          </Grid2>
         </Grid2>
-        <Grid2 size={8} style={{ textAlign: "end" }}>
-          <Button variant="outlined" color="secondary">
-            Add Todo
-          </Button>
-        </Grid2>
-      </Grid2>
-    </AppBar>
+      </AppBar>
+      <TodoCard open={openDialog} onClose={handleCloseDialog} />   
+    </React.Fragment>
   );
 }
 
--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,11 +1,68 @@
-import React from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+import { TextField } from '@mui/material';
 
-/**
- * Design Todo Card which accepts the props like taskName, taskDescription, date and time, section .
- * @returns
- */
-const TodoCard = () => {
-  return <div>Todo Card</div>;
-};
+interface TodoCardProps {
+  open: boolean;           
+  onClose: () => void;     
+}
 
-export default TodoCard;
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function TodoCard({ open, onClose }: TodoCardProps) {
+  return (
+    <Dialog
+      open={open}             
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={onClose}       
+      aria-describedby="alert-dialog-slide-description"
+      PaperProps={{
+        component: 'form',
+        onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const formJson = Object.fromEntries((formData as any).entries());
+          const email = formJson.email;
+          console.log(email);
+          onClose();             
+        },
+      }}
+    >
+      <DialogTitle>Add ToDo</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Please enter the name of the task.
+        </DialogContentText>
+        <TextField
+          autoFocus
+          required
+          margin="dense"
+          id="Task-Name"
+          name="Name"
+          label="Task Name"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
